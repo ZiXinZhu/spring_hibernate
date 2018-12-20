6a32e26af43f10f9dc03d50134dcd31a45f73ae1
John <1101648204@qq.com>
2018-12-20 22:19:02 +0800

    SpringMVC+Hibernate+REST风格

--- src/main/java/com/zzx/conteoller/UserController.java
+++ src/main/java/com/zzx/conteoller/UserController.java
@@ -1,12 +1,18 @@
 package com.zzx.conteoller;
 
+import com.zzx.dao.GoodsDao;
+import com.zzx.dao.UserDao;
 import com.zzx.entity.GoodsEntity;
+import com.zzx.entity.UserEntity;
 import com.zzx.listenerfilterintercept.inter.Permission;
 import com.zzx.service.GoodsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 /**
  * Created by Mr.John on 2018/9/17 16:02.
@@ -16,6 +22,8 @@ public class UserController {
 
     @Autowired
     GoodsService service;
+    @Autowired
+    GoodsDao goodsDao;
 
     @RequestMapping("/hello")
     public String hello(){
@@ -27,5 +35,11 @@ public class UserController {
     public ModelAndView save(GoodsEntity entity){
         service.save(entity);
         return new ModelAndView( "index");
+    }
+    @ResponseBody
+    @RequestMapping("/all")
+    public List<UserEntity> all(){
+        System.out.println(goodsDao.all());
+        return goodsDao.all();
     }
 }
--- src/main/java/com/zzx/dao/GoodsDao.java
+++ src/main/java/com/zzx/dao/GoodsDao.java
@@ -1,11 +1,15 @@
 package com.zzx.dao;
 
 import com.zzx.entity.GoodsEntity;
+import com.zzx.entity.UserEntity;
+import org.hibernate.SQLQuery;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Created by Mr.John on 2018/9/17 16:27.
@@ -26,5 +30,19 @@ public class GoodsDao {
         transaction.commit();//事务提交
         session.close();//关闭会话
         sessionFactory.close();//关闭会话工厂
+    }
+
+    public List<UserEntity> all(){
+        Configuration configuration=new Configuration().configure();
+        sessionFactory=configuration.buildSessionFactory();
+        session=sessionFactory.openSession();
+        transaction=session.beginTransaction();
+        String sql="select * from user";
+        SQLQuery sqlQuery=session.createSQLQuery(sql);
+        List<UserEntity> list=sqlQuery.list();
+        transaction.commit();
+        session.close();
+        sessionFactory.close();
+        return list;
     }
 }
